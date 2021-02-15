5f45621a9c344d049d02026acf1cd7861bf0d20d
Brian Runnells <1672302+Dhaulagiri@users.noreply.github.com>
2021-02-15 13:58:48 -0600

    remove tailwind for now

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,12 +3,28 @@ import Controller from '@ember/controller';
 export default class IndexController extends Controller {
   columns = [
     {
+      name: `Date`,
+      valuePath: `date`
+    },
+    {
       name: `Climb`,
       valuePath: `Climb`
     },
     {
       name: `Name`,
       valuePath: `name`
+    },
+    {
+      name: `Grade`,
+      valuePath: `Grade`
+    },
+    {
+      name: `Area`,
+      valuePath: `Area`
+    },
+    {
+      name: `Type`,
+      valuePath: `Type`
     }
   ]
 }
